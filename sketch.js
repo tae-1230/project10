491cfdc64d6c2a33468b1bceae24572d44c30653
tae-1230 <74037846+tae-1230@users.noreply.github.com>
2022-01-21 16:43:34 -0800

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -4,11 +4,8 @@ var seaImg,shipImg;
 function preload(){
   //uncomment the code to add animation to ship 
 
-  shipImg1 = loadAnimation("ship-1.png");
-  shipImg1 = loadAnimation("ship-1.png");
-  shipImg1 = loadAnimation("ship-1");
+
   shipImg1 = loadAnimation("ship-1.png","ship-2.png","ship-1.png","ship-2.png");
-  shipImg1 = loadAnimation("ship-1","ship-2","ship-1","ship-2");
   
   seaImg = loadImage("sea.png");
 }
